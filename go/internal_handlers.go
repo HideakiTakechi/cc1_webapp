9b00b74e7ad91e718c0a54b5db13e65c0d62ab9c
Eri5yn4ck <130022850+Eri5yn4ck@users.noreply.github.com>
2024-12-08 07:26:54 +0000

    score:3394 ランダムで探していたのを修正

--- go/internal_handlers.go
+++ go/internal_handlers.go
@@ -21,28 +21,58 @@ func internalGetMatching(w http.ResponseWriter, r *http.Request) {
 	}
 
 	matched := &Chair{}
-	empty := false
-	for i := 0; i < 10; i++ {
-		if err := db.GetContext(ctx, matched, "SELECT * FROM chairs INNER JOIN (SELECT id FROM chairs WHERE is_active = TRUE ORDER BY RAND() LIMIT 1) AS tmp ON chairs.id = tmp.id LIMIT 1"); err != nil {
-			if errors.Is(err, sql.ErrNoRows) {
-				w.WriteHeader(http.StatusNoContent)
-				return
-			}
-			writeError(w, http.StatusInternalServerError, err)
-		}
 
-		if err := db.GetContext(ctx, &empty, "SELECT COUNT(*) = 0 FROM (SELECT COUNT(chair_sent_at) = 6 AS completed FROM ride_statuses WHERE ride_id IN (SELECT id FROM rides WHERE chair_id = ?) GROUP BY ride_id) is_completed WHERE completed = FALSE", matched.ID); err != nil {
-			writeError(w, http.StatusInternalServerError, err)
+	//sato GPT変更
+	if err := db.GetContext(ctx, matched, `
+		SELECT *
+		FROM chairs
+		WHERE is_active = TRUE
+		AND NOT EXISTS (
+			SELECT 1
+			FROM ride_statuses
+			WHERE ride_id IN (
+			SELECT id
+			FROM rides
+			WHERE chair_id = chairs.id
+			)
+			GROUP BY ride_id
+			HAVING COUNT(chair_sent_at) < 6
+		)
+		ORDER BY id
+		LIMIT 1
+	`); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			w.WriteHeader(http.StatusNoContent)
 			return
 		}
-		if empty {
-			break
-		}
-	}
-	if !empty {
-		w.WriteHeader(http.StatusNoContent)
+		writeError(w, http.StatusInternalServerError, err)
 		return
 	}
+
+	//変更前コード
+	// empty := false
+	// for i := 0; i < 10; i++ {
+	// 	if err := db.GetContext(ctx, matched, "SELECT * FROM chairs INNER JOIN (SELECT id FROM chairs WHERE is_active = TRUE ORDER BY RAND() LIMIT 1) AS tmp ON chairs.id = tmp.id LIMIT 1"); err != nil {
+	// 		if errors.Is(err, sql.ErrNoRows) {
+	// 			w.WriteHeader(http.StatusNoContent)
+	// 			return
+	// 		}
+	// 		writeError(w, http.StatusInternalServerError, err)
+	// 	}
+
+	// 	if err := db.GetContext(ctx, &empty, "SELECT COUNT(*) = 0 FROM (SELECT COUNT(chair_sent_at) = 6 AS completed FROM ride_statuses WHERE ride_id IN (SELECT id FROM rides WHERE chair_id = ?) GROUP BY ride_id) is_completed WHERE completed = FALSE", matched.ID); err != nil {
+	// 		writeError(w, http.StatusInternalServerError, err)
+	// 		return
+	// 	}
+	// 	if empty {
+	// 		break
+	// 	}
+	// }
+	// if !empty {
+	// 	w.WriteHeader(http.StatusNoContent)
+	// 	return
+	// }
+	//sato変更ここまで
 
 	if _, err := db.ExecContext(ctx, "UPDATE rides SET chair_id = ? WHERE id = ?", matched.ID, ride.ID); err != nil {
 		writeError(w, http.StatusInternalServerError, err)
